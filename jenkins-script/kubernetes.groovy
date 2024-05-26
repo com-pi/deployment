0df047ca39ea331193262a46908be316528b2f85
kihong_kim <berserkppp1@gmail.com>
2024-05-27 02:10:53 +0900

    [fix] 스크립트 수정

--- jenkins-script/kubernetes.groovy
+++ jenkins-script/kubernetes.groovy
@@ -13,6 +13,10 @@ pipeline {
                     git branch: 'main', changelog: false, credentialsId: 'deployment-key', poll: false, url: 'git@github.com:com-pi/deployment.git'
 
                     sh 'kubectl apply -f k8s/nginx/nginx-deployment.yaml'
+                    sh 'kubectl apply -f k8s/nginx/nginx-service.yaml'
+
+                    sh 'kubectl apply -f k8s/api-gateway/api-gateway-deployment.yaml'
+                    sh 'kubectl apply -f k8s/api-gateway/api-gateway-service.yaml'
                 }
             }
         }
