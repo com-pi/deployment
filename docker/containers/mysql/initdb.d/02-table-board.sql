18a62c88e666acb4c1ddf66c5379bbc8a547d079
nagool <quietwave00@gmail.com>
2024-06-11 20:56:39 +0900

    [feat] board-service DDL 스크립트 추가

--- docker/containers/mysql/initdb.d/02-table-board.sql
+++ docker/containers/mysql/initdb.d/02-table-board.sql
@@ -1,25 +1,58 @@
 USE board;
 
-CREATE TABLE article (
-                         price integer,
-                         id bigint auto_increment not null,
-                         member_id bigint unique,
-                         article_type varchar(255) check (article_type in ('BUY_AND_SELL','PLANT_SITTER','SHOW_OFF','QNA')),
-                         content varchar(255),
-                         eupmyundong varchar(255),
-                         hashtags varchar(255),
-                         image_urls varchar(255),
-                         location Geometry,
-                         sido varchar(255),
-                         sigungu varchar(255),
-                         title varchar(255),
-                         primary key (id));
+create table article
+(
+    article_id   bigint       not null  primary key,
+    view_count   int          null,
+    created_at   datetime(6)  not null,
+    deleted_at   datetime(6)  null,
+    member_id    bigint       null,
+    updated_at   datetime(6)  null,
+    article_type varchar(31)  not null,
+    content      varchar(255) null,
+    deletion_yn  varchar(255) null,
+    image_urls   varchar(255) null,
+    title        varchar(255) null,
+    foreign key (member_id) references member (member_id)
+) ENGINE = InnoDB;
 
-CREATE TABLE member (
-                        id bigint auto_increment not null,
-                        member_id bigint unique,
-                        image_url varchar(255),
-                        nickname varchar(255),
-                        primary key (id));
+create table article_seq
+(
+    next_val bigint null
+) ENGINE = InnoDB;
 
-INSERT INTO member (member_id, image_url, nickname) VALUES (1, null, '관리자');
+create table buy_and_sell
+(
+    article_id  bigint       not null   primary key,
+    is_free     bit          null,
+    like_count  int          null,
+    price       int          null,
+    eupmyundong varchar(255) null,
+    hashtags    varchar(255) null,
+    sido        varchar(255) null,
+    sigungu     varchar(255) null,
+    location    geometry     null,
+    foreign key (article_id) references article (article_id)
+) ENGINE = InnoDB;
+
+create table likes
+(
+    like_id      bigint auto_increment                             primary key,
+    is_liked     bit                                                      null,
+    article_id   bigint                                                   null,
+    member_id    bigint                                                   null,
+    article_type enum ('BUY_AND_SELL', 'PLANT_SITTER', 'SHOW_OFF', 'QNA') null,
+    unique (article_id, member_id),
+    foreign key (article_id) references article (article_id),
+    foreign key (member_id) references member (member_id)
+) ENGINE = InnoDB;
+
+
+create table member
+(
+    member_id bigint auto_increment primary key,
+    image_url varchar(255) null,
+    nickname  varchar(255) null
+) ENGINE = InnoDB;
+
+
